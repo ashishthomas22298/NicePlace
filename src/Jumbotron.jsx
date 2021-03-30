94c2c1c53e05d9f03646dca0f2028c1d6c0d53d7
ashishthomas22298 <athomas22298@gmail.com>
2021-03-30 17:15:15 -0400

    Jumbotron updates

--- src/Jumbotron.jsx
+++ src/Jumbotron.jsx
@@ -3,9 +3,25 @@ import "./Jumbotron.css";
 
 function Jumbotron(){
     return<div id="Jumbotron">
-            <div>
+            <div className="Title">
                 <h1>Space that</h1>
                 <h1>inspire</h1>
+            </div>
+
+            <div className="options">
+                <div className="location">
+                    <h3>Location</h3>
+                    <p>London</p>
+                </div>
+                <div className="activity">
+                    <h3>Activity</h3>
+                    <p>Relaxing</p>
+                </div>
+                <div className="date">
+                    <h3>Date</h3>
+                    <p>03-30-21</p>
+                </div>
+                <div className="search">Search</div>
             </div>
         </div>;
 }
