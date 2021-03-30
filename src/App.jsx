5bc91fbe1390ae13be23ec9d07401c2aa0918ad1
ashishthomas22298 <athomas22298@gmail.com>
2021-03-30 14:04:18 -0400

    Jumbotron structure complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import "./App.css";
 import Header from "./Header.jsx";
+import Jumbotron from "./Jumbotron.jsx";
 
 function App(){
     return <div>
         <Header />
-        <section id="Home"></section>
+        <section id="Home" >
+            <Jumbotron />
+        </section>
         <section id="About"></section>
         <section id="Services"></section>
         <section id="Blog"></section>
---
+++ src/Jumbotron.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Jumbotron.css";
+
+function Jumbotron(){
+    return<div id="Jumbotron">
+            <div>
+                <h1>Space that</h1>
+                <h1>inspire</h1>
+            </div>
+        </div>;
+}
+
+export default Jumbotron;
